0f909851e4f650cd4fa2acb4a9f07b1e2378cd8b
lilijka333 <malgorzata.pilch12@gmail.com>
2022-04-23 11:49:35 +0200

    Correct definition

--- lista.py
+++ lista.py
@@ -1,19 +1,18 @@
-def simple_list(data: list) -> list:
-    simple_list = []
+from typing import List, Union
 
-    for i in data:
-        try:
-            for j in i:
-                simple_list.append(j)
-        except:
-            simple_list.append(i)
+
+def flatten_list(lst: List[Union[List, int ]]) -> List[int]:
+    if not lst:
+        return lst
+    result = []
+    for element in lst:
+        if isinstance(element, int):
+            result.append(element)
+        else:
+            result += flatten_list(element)
+    return result
+
 
 example = [1, [2, [3, 4]], 5, [6, 7], 8, [9, [10]]]
 
-# print(simple_list(example))
-
-
-def flat_list(lst):
-    return [item for sublist in lst for item in sublist]
-
-print(flat_list(example))
+print(flatten_list(example))
